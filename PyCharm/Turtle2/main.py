5f3ad77820b18b75bb7e0b77d4986691ca36d5a9
Venkat sushanth kollapaneni <k.venkat.sushanth@gmail.com>
2023-10-06 19:04:27 +0530

    hi

--- PyCharm/Turtle2/main.py
+++ PyCharm/Turtle2/main.py
@@ -9,14 +9,33 @@ t.pencolor("black")
 s.bgcolor("grey")
 
 # Speed of Turtle
-t.speed(1000)
+t.speed(0)
 
 # Variables used for the drawing
-UnitWidth = int(input("What is the width of the square? "))
+UnitWidth = 5
+# int(input("What is the width of the square? "))
 Width = 20
 Side = UnitWidth * Width
 
+
 # Functions
+def ColorGrid(row, column):
+    x = (column - 1) * Width
+    y = (row - 1) * Width
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    for a in range(0, Width // 2):
+        t.left(90)
+        t.fd(1)
+        t.right(90)
+        t.fd(Width)
+        t.left(90)
+        t.fd(1)
+        t.left(90)
+        t.fd(Width)
+        t.left(180)
+
 
 # Drawing
 
@@ -47,6 +66,20 @@ for k in range(0, UnitWidth - 1):
     t.left(180)
     t.fd(Side)
     t.right(90)
+
+OddRows = (UnitWidth + 1) // 2
+EvenRows = UnitWidth // 2
+for r1 in range(0, OddRows):
+    for c1 in range(0, OddRows):
+        row1 = r1 * 2 + 1
+        col1 = c1 * 2 + 1
+        ColorGrid(row1, col1)
+
+for r2 in range(0, EvenRows):
+    for c2 in range(0, EvenRows):
+        row2 = r2 * 2 + 2
+        col2 = c2 * 2 + 2
+        ColorGrid(row2, col2)
 
 # Returns back to home
 t.penup()
