ef296e28fe7b4b6a985cf68427546b0e42efb4ef
Venkat sushanth kollapaneni <k.venkat.sushanth@gmail.com>
2023-10-06 18:24:20 +0530

    hi

--- PyCharm/Turtle2/main.py
+++ PyCharm/Turtle2/main.py
@@ -1,23 +1,33 @@
 import turtle
 
+# Assigning variables
 t = turtle.Turtle()
 s = turtle.Screen()
 
+# Setting Board
 t.pencolor("black")
 s.bgcolor("grey")
 
+# Speed of Turtle
 t.speed(1000)
 
+# Variables used for the drawing
 UnitWidth = int(input("What is the width of the square? "))
 Width = 20
 Side = UnitWidth * Width
 
+# Functions
+
+# Drawing
+
+# The square
 for i in range(0, 4):
     t.fd(Side)
     t.left(90)
 
 t.left(90)
 
+# The rows
 for j in range(0, UnitWidth - 1):
     t.fd(Width)
     t.right(90)
@@ -29,6 +39,7 @@ for j in range(0, UnitWidth - 1):
 t.fd(Width)
 t.right(90)
 
+# The columns
 for k in range(0, UnitWidth - 1):
     t.fd(Width)
     t.right(90)
@@ -37,7 +48,9 @@ for k in range(0, UnitWidth - 1):
     t.fd(Side)
     t.right(90)
 
+# Returns back to home
 t.penup()
 t.home()
 
+# Exits the turtle graphic screen only when you click on it
 turtle.exitonclick()
